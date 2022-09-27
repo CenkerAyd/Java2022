1f227e3a0a1bd6e9a83844b17aca9e3762e18d3e
cenk1903 <cenkeraydincse@gmail.com>
2022-09-27 21:05:57 +0300

    Classlar ile ilgili çalışıldı.

---
+++ src/ClassPratik.java
@@ -0,0 +1,21 @@
+public class ClassPratik {
+    public  int Plus(int sayi1, int sayi2){
+    int result =sayi1+sayi2;
+    return result;
+    }
+
+    public int minus(int sayi1, int sayi2){
+        int result =sayi1-sayi2;
+        return result;
+    }
+    public int multiplication (int sayi1,int sayi2){
+        int result=sayi1*sayi2;
+        return result;
+    }
+
+    public int division(int sayi1,int sayi2){
+        int result =sayi1/sayi2;
+        return result;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        ClassPratik classPratik=new ClassPratik();
+        int plus =classPratik.Plus(4,5);
+        int minus =classPratik.minus(4,5);
+        int division =classPratik.division(4,5);
+        int multiplication =classPratik.multiplication(4,5);
+
+        System.out.println(plus);
+        System.out.println(minus);
+        System.out.println(division);
+        System.out.println(multiplication);
+
+    }
+}
